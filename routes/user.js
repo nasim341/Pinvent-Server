e4fcb886274e015f173da48c1f82ab624e1f9a88
Md. Nasim Ahmed <mdnasimahmed341@gmail.com>
2023-01-10 12:53:28 -0500

    get user

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const { registerUser, loginUser, logout } = require("../controllers/user")
+const { registerUser, loginUser, logout, getUser } = require("../controllers/user")
 
 router.post("/register", registerUser)
 router.post("/login", loginUser)
+router.post("/getUser", getUser)
     //router.post("/logout", logout)
 module.exports = router;
