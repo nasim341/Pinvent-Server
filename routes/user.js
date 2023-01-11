0620d9dcfd3145eb37ffc51a2356dbe46f88714e
Md. Nasim Ahmed <mdnasimahmed341@gmail.com>
2023-01-11 12:51:13 -0500

    update

--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,7 @@ const { registerUser, loginUser, logout, getUser } = require("../controllers/use
 
 router.post("/register", registerUser)
 router.post("/login", loginUser)
+router.post("/logout", logout)
 router.post("/getUser", getUser)
     //router.post("/logout", logout)
 module.exports = router;
